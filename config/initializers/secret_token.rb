1e781df42c07a7245fa26bdae177975990a68eab
Peter Sobot <psobot@gmail.com>
2012-03-01 16:31:12 -0500

    Moved secret keys to settings.yml.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SEHackDay::Application.config.secret_token = 'dee9b8e8ee187e03a0f4fdcde4553c097405b9c5772a3667ad51daa442cbbd9f2a75065d24dd433bebcc03857539344cb11cd3e7696dabb3555925b6b49fa741'
+settings_file = File.expand_path('../settings.yml', File.dirname(__FILE__))
+settings = YAML::load(File.open(settings_file))
+env = Rails.env
+SEHackDay::Application.config.secret_token = settings[env]['secret_token']
